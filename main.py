16f2fbd9abc8f1991486569f5a046f747e8f2b18
emilien.arnaud <emilien.arnaud@epitech.eu>
2025-12-13 18:14:29 +0100

    handler event , handle text creation , handle frame creation , handle image creation

--- main.py
+++ main.py
@@ -2,25 +2,24 @@
 import pygame.display
 from pygame import Vector2
 
+from Class.text import game_text
 from src.Class.ClickButton import ClickButton
 from src.Class.main import main
-from src.Class.asset import assethandler
+from src.Class.asset import assethandler, fonthandler
+from src.Class.text import game_text, format_num
+
+pygame.init()
+pygame.font.init()
 
 Window = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Tree clicker")
 ASSET_HANDLER = assethandler().init()
-GAME = main(ASSET_HANDLER)
-
-def format_num(x: float) -> str:
-    if x < 1000:
-        return f"{x:.0f}"
-    for suffix, div in (("K", 1e3), ("M", 1e6), ("B", 1e9), ("T", 1e12)):
-        if x < div * 1000:
-            return f"{x / div:.2f}{suffix}"
-    return f"{x:.2e}"
+FONT_HANDLER = fonthandler()
+GAME = main(ASSET_HANDLER, FONT_HANDLER)
 
 GAME.add_event(GAME.quit)
 GAME.add_drawable(ClickButton(GAME, Vector2(x=400, y=400)))
+GAME.add_text(game_text("money",GAME, Vector2(x=100, y=0),"money: " +  format_num(GAME.money)))
 
 while GAME.isRunning:
     dt = GAME.clock.tick(GAME.FPS) / 1000.0
